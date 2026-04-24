684d293fe652fe68054f417e1e1e97d7693bd0c4
dianaerazo <00149823@uca.edu.sv>
2026-04-24 11:19:19 -0600

    agregar runner en PokemonApplication

--- pokedex/src/main/java/com/example/pokedex/PokedexApplication.java
+++ pokedex/src/main/java/com/example/pokedex/PokedexApplication.java
@@ -1,8 +1,13 @@
 package com.example.pokedex;
 
-
+import com.example.pokedex.model.Pokemon;
+import com.example.pokedex.service.PokemonService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class PokedexApplication {
@@ -11,5 +16,26 @@ public class PokedexApplication {
         SpringApplication.run(PokedexApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run(PokemonService service) {
+        return args -> {
+
+            System.out.println("=== FILTRO POR TIPO ===");
+            imprimir(service.filtrarPorTipo("Agua"));
+
+            System.out.println("=== FILTRO POR ZONA ===");
+            imprimir(service.filtrarPorZona("Bosque"));
+
+            System.out.println("=== FILTRO POR DEBILIDAD ===");
+            imprimir(service.filtrarPorDebilidad("Tierra"));
+        };
+    }
+
+    private void imprimir(List<Pokemon> pokemones) {
+        pokemones.forEach(p ->
+                System.out.println("[PKMN] Nombre: " + p.getNombre()
+                        + " | Tipo: " + p.getTipo()
+                        + " | Debilidades: " + String.join(", ", p.getDebilidades()))
+        );
     }
 }
